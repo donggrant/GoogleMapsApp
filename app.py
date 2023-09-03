aebb9af79693a713e9633667596b43786153f960
Grant Dong <grant.dong@datadoghq.com>
2023-09-03 12:39:12 -0400

    add error handling

--- app.py
+++ app.py
@@ -14,12 +14,15 @@ def index():
 @app.route("/convert", methods=["POST"])
 def convert():
     data = request.get_json()
-    if data["direction"] == "1":
-        lat, lng = get_lat_lng(data["input"], API_KEY)
-        return jsonify({"lat": lat, "lng": lng})
-    elif data["direction"] == "2":
-        address = get_address_from_lat_lng(data["lat"], data["lng"], API_KEY)
-        return jsonify({"address": address})
+    try:
+        if data["direction"] == "1":
+            lat, lng = get_lat_lng(data["input"], API_KEY)
+            return jsonify({"lat": lat, "lng": lng})
+        elif data["direction"] == "2":
+            address = get_address_from_lat_lng(data["lat"], data["lng"], API_KEY)
+            return jsonify({"address": address})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 def get_lat_lng(address, api_key):
     endpoint = "https://maps.googleapis.com/maps/api/geocode/json"
@@ -28,7 +31,7 @@ def get_lat_lng(address, api_key):
     if response.status_code == 200 and response.json()["status"] == "OK":
         location = response.json()["results"][0]["geometry"]["location"]
         return location["lat"], location["lng"]
-    return None, None
+    raise ValueError("Failed to convert the given input. Please ensure it's correct.")
 
 def get_address_from_lat_lng(lat, lng, api_key):
     endpoint = "https://maps.googleapis.com/maps/api/geocode/json"
@@ -36,7 +39,7 @@ def get_address_from_lat_lng(lat, lng, api_key):
     response = requests.get(endpoint, params=params)
     if response.status_code == 200 and response.json()["status"] == "OK":
         return response.json()["results"][0]["formatted_address"]
-    return None
+    raise ValueError("Failed to convert the given input. Please ensure it's correct.")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5100)
+    app.run(debug=True, port=5101)
